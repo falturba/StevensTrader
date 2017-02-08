470d65257277b43f8985943afff5fc47cb1b8734
EXIT <exitudio@gmail.com>
2017-02-08 12:21:24 -0500

    save signup react [with bugs]

--- views/pages/Signup.js
+++ views/pages/Signup.js
@@ -27,6 +27,23 @@ export default class Signup extends React.Component{
             this.state.isPasswordRetypeCorrect
         ){
             //submit
+            var newName = 'John Smith',
+            xhr = new XMLHttpRequest();
+
+            xhr.open('POST', 'http://localhost:3000/services/signup');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = function() {
+                if (xhr.status === 200 && xhr.responseText !== newName) {
+                    alert('Something went wrong.  Name is now ' + xhr.responseText);
+                }else if (xhr.status !== 200) {
+                    alert('Request failed.  Returned status of ' + xhr.status);
+                }
+            };
+            xhr.send(encodeURI( 
+                NAME+"="+this.state.name+"&"+
+                EMAIL+"="+this.state.email+"&"+
+                PASSWORD+"="+this.state.password+"&"
+            ));
         }
     }
 
