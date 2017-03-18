8e650e406b2fe80a3c9b4ec8fa9cd307ff89b17b
Fawaz AlTurbaq <falturba@stevens.edu>
2017-03-18 18:29:58 -0400

    authentication in get products

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,13 +1,31 @@
 import express from 'express'
 import Item from '../models/item'
+import jwt from 'jsonwebtoken';
+import config from '../config/jwtConfig';
+
 const router = express.Router();
 router.get('/getproducts',(req,res)=>{
     // console.dir(req)
+    var token = req.headers["token"]
+    if(token)
+    {
+    	jwt.verify(token, config.jwtSecret, function(err, decoded) {      
+    		if (err) {
+    			    res.json({ success: false, message: 'Failed to authenticate token.' });    
+    		} else {
+    			Item.find({}, function(error, data){
+    				res.status(200).json({products:data});
+    			});
+    		}
 
-    Item.find({}, function(error, data){
-        res.status(200).json({products:data});
-    })
     // res.writeHead(200, {'Content-Type': 'application/json'})
     // res.json({status:'ok'})
-})
+			});
+    }else {
+    	return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.'
+        });
+    } 
+    });
 module.exports = router
