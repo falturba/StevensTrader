e2175075dd36386940dcf47b1d67d08071e9b109
EXIT <exitudio@gmail.com>
2017-04-02 19:37:43 -0400

    remove listitem from import

--- dev/src/services/index.js
+++ dev/src/services/index.js
@@ -1,6 +1,5 @@
 import getProduct from './getProduct'
 import getProducts from './getProducts'
-import listItem from './listItem'
 import login from './login'
 import postProduct from './postProduct'
 import signup from './signup'
@@ -9,7 +8,6 @@ export function initServices(app){
   console.log("init service...")
   app.use('/services',getProduct)
   app.use('/services',getProducts)
-  app.use('/services',listItem)
   app.use('/services',login)
   app.use('/services',postProduct)
   app.use('/services',signup)
