628bc6e90702017afeb6e35bb0cc8cd9a4abc0b1
kaldrich15 <kaldric1@stevens.edu>
2017-04-17 08:55:24 -0400

    Add files via upload

---
+++ traderAdmin/config.py
@@ -0,0 +1,6 @@
+from admin import admin
+
+admin.config['MONGO_DBNAME']="stevenstradersystem"
+admin.config['MONGO_URI']="mongodb://dbuser:strader%40123@ds139959.mlab.com:39959/stevenstradersystem"
+
+#database connection
