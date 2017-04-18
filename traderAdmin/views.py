1612e956b450436c9e782acea015fb92999887fa
kaldrich15 <kaldric1@stevens.edu>
2017-04-18 17:27:48 -0400

    templates/file structuring

--- traderAdmin/views.py
+++ traderAdmin/views.py
@@ -1,9 +1,9 @@
-from admin import admin
-from admin import mongo
+from admin import admin,mongo
 from flask import render_template # necessary libraries and files
 
+#db.accounts.find({},{name:1, email: 1, _id:0})
 @admin.route('/users')
 def users():
-    accounts= mongo.db.accounts.find()# query for user info
-    return render_template('indexA.html',
-                            accounts=accounts)# render html in users route
+    accts= mongo.db.accounts.find({},{'name':1, 'email': 1, '_id':0})# query for user info 
+    return render_template('index.html',
+                            accounts=accts)# render html in users route
