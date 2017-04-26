5d59ab9c517b19a269d8ce6de63ebdda5c640a9f
Fawaz AlTurbaq <falturba@stevens.edu>
2017-04-26 03:06:25 -0400

    Adding Active and Blocked account status

--- dev/src/models/enum.js
+++ dev/src/models/enum.js
@@ -1,4 +1,5 @@
 export const condintionEnum = ['New','Refurbished','Used','Very Good','Good','Acceptable'];
 export const categoryEnum = ['Electronics','Books','Bikes','Video Games','Bags','Misc'];
+export const statusEnum = ['Active','Blocked']
 export const priceMin = 0;
 export const priceMax = 5000;
